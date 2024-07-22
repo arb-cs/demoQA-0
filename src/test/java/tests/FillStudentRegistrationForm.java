e61e3b7a42c70995183120e6e6df85ead6a601b6
arb-cs <trust1one@xfiles.com>
2024-07-22 23:42:51 +0300

    Renamed file and added to files for future tests.

--- src/test/java/tests/FillStudentRegistrationForm.java
+++ src/test/java/tests/FillStudentRegistrationForm.java
@@ -7,7 +7,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class FillStudentRegistrationFormPageObject extends TestBase {
+public class FillStudentRegistrationForm extends TestBase {
 
     RegistrationPage registrationPage = new RegistrationPage();
 
